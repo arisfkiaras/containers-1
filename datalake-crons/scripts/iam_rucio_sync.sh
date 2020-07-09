ba8de8e4c9e1e72c1d096c3f731a66be98fe9c94
Aristeidis Fkiaras <aristeidis.fkiaras@cern.ch>
2020-07-09 15:53:02 +0200

    add wip

---
+++ datalake-crons/scripts/iam_rucio_sync.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SLEEP_TIME_MINUTES=${IAM_RUCIO_SYNC_SLEEP_TIME_MINUTES:-120m}
+
+echo 'Initializing Sync to IAM Loop'
+
+# TODO: Check all required files are configured here.
+echo '$IAM_RUCIO_SYNC_SLEEP_TIME_MINUTES'
+
+export IAM_SERVER=$IAM_RUCIO_SYNC_SERVER
+
+export IAM_CLIENT_ID=$IAM_RUCIO_SYNC_CLIENT_ID
+
+export IAM_CLIENT_SECRET=$IAM_RUCIO_SYNC_CLIENT_SECRET
+
+
+echo 'Initializing Sync to IAM Loop'
+echo 'Initializing Sync to IAM Loop'
+echo 'Initializing Sync to IAM Loop'
+
+while true; do
+    python Utilities-and-Operations-Scripts/iam-rucio-sync/sync_iam_rucio.py
+    echo 'Will sleep for $SLEEP_TIME_MINUTES'
+    sleep $SLEEP_TIME_MINUTES
+done
